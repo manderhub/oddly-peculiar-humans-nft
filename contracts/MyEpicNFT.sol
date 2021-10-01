ccf87c647358bfafc6e63ff09be3c8fd2223c80b
mario <mander.and@gmail.com>
2021-10-01 23:03:40 +0200

    randomized nft creation implemented

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -13,9 +13,30 @@ contract MyEpicNFT is ERC721URIStorage {
     // Counters.Counter is a struct, that can be incremented, decremented and reset
     Counters.Counter private _tokenIds;
 
+    // base SVC code string
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+    
+    // 2-dimensional array of 3 dynamic arrays
+    string[][3] wordLibrary;
+
+    function generatePseudoRandomIndex(string memory _input) private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, _input)));
+    }
+
+    function generateRandomTriple(uint _input) private view returns (string memory) {
+        string memory triple = "";
+        for(uint i = 0; i < wordLibrary.length; i++) {
+            string memory word = wordLibrary[i][generatePseudoRandomIndex(string(abi.encodePacked(triple, Strings.toString(_input)))) % wordLibrary[i].length];
+            triple = string(abi.encodePacked(triple, word));
+        }
+        return triple;
+    }
+
     // ERC721 ("SquareNFT", "SQUARE") "calls" the constructor or the parent contract ERC721
     constructor() ERC721 ("SquareNFT", "SQUARE") {
-        console.log("Bro, this NFT will be a banger!");
+        wordLibrary[0] = ["Abnormally","Abruptly","Absently","Accidentally","Accusingly","Actually","Adventurously","Adversely","Amazingly","Angrily","Anxiously","Arrogantly","Awkwardly","Badly","Bashfully"];
+        wordLibrary[1] = ["Abundant","Accurate","Addicted","Adorable","Adventurous","Afraid","Aggressive","Alcoholic","Alert","Aloof","Ambitious","Ancient","Angry","Animated","Annoying"];
+        wordLibrary[2] = ["Boris","Aleksandr","Igor","Nikita","Timofey","Vadim","Ilya","Jaroslav","Ruslan","Anastasia","Ekaterina","Alyona","Mila","Viktoria","Sasha","Inessa"];
     }
 
     // A function our user will hit from the front-end to get their NFT
@@ -25,11 +46,17 @@ contract MyEpicNFT is ERC721URIStorage {
         // Used to keep track of the NFTs unique identifier.
         uint256 newItemId = _tokenIds.current();
 
+        string memory tripleWord = generateRandomTriple(newItemId);
+        string memory finalSvg = string(abi.encodePacked(baseSvg, tripleWord, "</text></svg>"));
+        console.log("\n-------------------");
+        console.log(finalSvg);
+        console.log("\n-------------------");
+
         // Mint the NFT with id newItemId to the user with address msg.sender.
         _safeMint(msg.sender, newItemId);
 
         // Set the NFTs unique identifier along with the data associated w/ that unique identifier.
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/P5VV");
+        _setTokenURI(newItemId, "PLACEHOLDER");
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
         // Increment the counter for when the next NFT is minted.
---
+++ scripts/deploy.js
@@ -0,0 +1,35 @@
+/*
+    run locally with: npx hardhat run scripts/deploy.js --network rinkeby
+*/
+// constant variable main is of type async function
+// behind main hides a lamda function which is async and has function body in between {}
+// we can call this variable just like we call a function: main()
+const main = async () => {
+    // compile contract and generate necessary files (e.g. in artifacts/)
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    // deploys the contract to local blockchain
+    const nftContract = await nftContractFactory.deploy();
+    // wait until contract is successfully mined on local blockchain
+    await nftContract.deployed();
+    // output the address of our contract on the local blockchain
+    console.log("Contract deployed to:", nftContract.address);
+
+    // Call the function
+    let txn = await nftContract.makeAnEpicNFT();
+    // wait for it to be minted
+    await txn.wait();
+    console.log("Minted NFT #1")
+
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
